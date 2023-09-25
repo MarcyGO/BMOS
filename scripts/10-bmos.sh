8bd995766bdfb5514e1d8241c34f50c5890b8223
Xinyu Wu <xinyuwu264@gmail.com>
2023-09-25 09:38:40 -0500

    update user name

--- scripts/10-bmos.sh
+++ scripts/10-bmos.sh
@@ -1,4 +1,4 @@
 # launch our autostart apps (if we are on the correct tty)
-if [ "`tty`" = "/dev/tty1" ] && [ "$USER" = "pi" ]; then
+if [ "`tty`" = "/dev/tty1" ] && [ "$USER" = "bmo" ]; then
     bash "/home/bmo/bmos/scripts/bmos.sh"
 fi
